af043a80bb417b6082e4b8092090db99649d64ed
Arley Souza <souzaarley12@gmail.com>
2023-05-02 16:01:18 -0300

    Criando movimento do rei

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -1,13 +1,13 @@
 ﻿namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-      
+
         public Peca(Tabuleiro tab, Cor cor)
         {
             this.posicao = null;
@@ -20,5 +20,9 @@
         {
             qteMovimentos++;
         }
+
+        public abstract bool[,] movimentosPossiveis();
+        
+       
     }
 }
